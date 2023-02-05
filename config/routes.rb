4a25bb482c74ee2bc1073888868b84ca9f9ee4dc
Ben Merryman <ben.merryman@gmail.com>
2023-02-05 00:26:28 -0500

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome_page/welcome'
+  root 'welcome_page#welcome'
   resources :trips
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
